436e93cc5c4735abf5318a1693398760912bf213
jae <wod6983@gmail.com>
2026-02-25 15:44:22 +0900

    Win32Window 객체로 창 생성 관련 기능 묶음
        - Win32Window 클래스 생성
        - Main에 작성된 창 생성 관련 코드 옮김
        - main 함수에서 Win32Window 사용해 창 생성하도록 처리

---
+++ Engine/Core/Win32Window.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <Windows.h>
+#include <string>
+#include <stdint.h>
+
+namespace Craft
+{
+    // WIN32 API로 생성하는 창 클래스.
+    class Win32Window
+    {
+    public:
+        Win32Window(uint32_t width = 1280, uint32_t height = 800, HINSTANCE instance = nullptr, WNDPROC messageProcedure = nullptr);
+        ~Win32Window();
+
+        bool Initialize();
+        // Getter.
+        inline uint32_t Width() const { return width; }
+        inline uint32_t Height() const { return height; }
+        inline HWND Handle() const { return handle; }
+
+    private:
+        // Title
+        std::wstring title;
+
+        // 창 들록 할 때 사용할 클래스 이름
+        std::wstring className = L"Craft Engine Class";
+
+        //size
+        uint32_t width = 0;
+        uint32_t height = 0;
+
+        HWND handle = nullptr;
+
+        HINSTANCE instance = nullptr;
+
+        WNDPROC messageProcedure = nullptr;
+    };
+}
+
---
+++ Engine/Main.cpp
@@ -0,0 +1,87 @@
+﻿#include "Core/Win32Window.h"
+
+// 콜백 함수.
+// 창 메시지 처리할 때 사용.
+LRESULT CALLBACK WindowProc(
+    HWND hwnd,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+);
+
+int main()
+{
+    // 그래픽스 프로그래밍.
+    // 창(Window)을 생성하고, API를 활용해서 창에 그림을 그리는 프로그램.
+    // Win32 API 활용.
+
+    // 운영체제에 창 등록할 때 사용할 클래스 이름 - 유일해야함.
+    std::wstring className = L"Craft Engine Window";
+    std::wstring title = L"Craft Engine";
+
+    // 클라이언트 영역(프레임) 크기.
+    uint32_t width = 1280;
+    uint32_t height = 800;
+
+    HINSTANCE hInstance = GetModuleHandle(nullptr);
+
+    Craft::Win32Window window(width, height, hInstance, WindowProc);
+
+    // 초기화 ( 초시화 실패 시 프로그램 종료).
+    if (!window.Initialize())
+    {
+	return -1;
+    }
+
+    // 창에서 발생하는 메시지 처리 루프.
+    // GetMessage - 동기 방식(Blocking 방식).
+    // PeekMessage - 비동기 방식(Non Blocking 방식).
+    // 동기: 처리되기 전까지 다른 동작 안함.
+    // 비동기: 처리되지 않아도 넘어감.
+    MSG msg = { };
+    // 창 종료 메시지가 발생할 때까지 실행.
+    while (msg.message != WM_QUIT)
+    {
+	// 창 메시지 처리.
+	// 비동기 방식으로 메시지가 들어왔는지 확인.
+	if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+	{
+	    // 전달받은 메시지를 적절하게 변환.
+	    TranslateMessage(&msg);
+
+	    // 변환한 메시지를 처리 함수에 전달.
+	    DispatchMessage(&msg);
+	}
+
+	// 창 메시지가 없으면 엔진 로직 처리.
+	else
+	{
+
+	}
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+	PostQuitMessage(0);
+	return 0;
+
+    case WM_PAINT:
+    {
+	PAINTSTRUCT ps;
+	HDC hdc = BeginPaint(hwnd, &ps);
+
+	// All painting occurs here, between BeginPaint and EndPaint.
+	FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+	EndPaint(hwnd, &ps);
+    }
+    return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
